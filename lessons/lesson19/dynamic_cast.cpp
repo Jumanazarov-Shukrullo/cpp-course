1d57257a6cef921a28e4e6e617ef76ea52340c09
Jumanazarov-Shukrullo <shukrullo.coder@gmail.com>
2023-02-01 18:37:52 +0500

    update repo

--- lessons/lesson19/dynamic_cast.cpp
+++ lessons/lesson19/dynamic_cast.cpp
@@ -12,88 +12,6 @@
 
 using namespace std;
 
-struct A {
-    virtual void print() {
-        std::cout << "A\n";
-    }
-};
-
-struct B : A {
-    void print() {
-        std::cout << "B\n";
-    }
-};
-
-struct C : A {
-    void print() {
-        std::cout << "C\n";
-    }
-};
-
-//struct Dog {
-//    virtual void bark() {
-//        std::cout << "bark...\n";
-//    }
-//
-//    virtual void eat() {
-//        std::cout << "dog is eating...\n";
-//    }
-//};
-//
-//struct Bird {
-//    virtual void chirp() {
-//        std::cout << "chirp...\n";
-//    }
-//
-//    virtual void eat() {
-//        std::cout << "bird is eating...\n";
-//    }
-//};
-//
-//struct DogBird : Dog, Bird {
-//    void eat() override {
-//        Dog::eat();
-//    }
-//
-//    using Dog::eat;
-//};
-
-//class Animal {
-//public:
-//    virtual void eat() = 0;
-//};
-//
-//class Dog : public Animal {
-//public:
-//    virtual void bark() { cout << "Woof!" << endl; }
-//
-//    void eat() override { cout << "The dog ate." << endl; }
-//};
-//
-//class Bird : public Animal {
-//public:
-//    virtual void chirp() { cout << "Chirp!" << endl; }
-//
-//    void eat() override { cout << "The bird ate." << endl; }
-//};
-//
-//class DogBird : public Dog, public Bird {
-//public:
-//    using Dog::eat;
-//};
-
-class Animal { public: virtual ~Animal() = default; };
-class Dog : public Animal {};
-class Bird : public Animal {};
-void speak(const Animal& animal)
-{
-    if (typeid(animal) == typeid(Dog)) {
-        cout << "Woof!" << endl;
-    } else if (typeid(animal) == typeid(Bird)) {
-        cout << "Chirp!" << endl;
-    }
-}
-
 
 struct Entity {
     virtual void print_name() {}
@@ -106,40 +24,15 @@ struct Player : Entity {
 struct Enemy: Entity {
 
 };
-/// One of the most compelling
-/// and simple uses of multiple inheritance is for the implementation
-/// of mixin classes.
+
+
 int main() {
-//    B b;
-//
-//    A *a = dynamic_cast<B *>(&b);
-//    C *c = dynamic_cast<C *>(a);
-//    if (c == nullptr) {
-//        std::cout << "no\n";
-//    } else {
-//        c->print();
-//        std::cout << "yes\n";
-//    }
-//    try {
-//        C &r1 = dynamic_cast<C &>(b);
-//    }
-//    catch (std::exception &e) {
-//        std::cout << e.what() << std::endl;
-//    }
-//    DogBird db;
-//    dynamic_cast<Dog &> (db).eat();
-//    db.eat();
-//    Derived1 dp1;
-//
-//    Base* bp = dynamic_cast<Base*>(&dp1);
-//
-//    Derived2* dp2 = dynamic_cast<Derived1*>(bp);
-//    Player* player = new Player();
-//    Entity* actual_enemy = new Enemy();
-//
-//    Entity* actual_player = player;
-//
-//    Player* p2 = dynamic_cast<Player*>(actual_player);
-//    Player* p3 = dynamic_cast<Player*>(actual_enemy);
+    Player* player = new Player();
+    Entity* actual_enemy = new Enemy();
+
+    Entity* actual_player = player;
+
+    Player* p2 = dynamic_cast<Player*>(actual_player);
+    Player* p3 = dynamic_cast<Player*>(actual_enemy);
     return 0;
 }
